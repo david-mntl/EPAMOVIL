7255527329d3d42700e477bb2384e3b4aff0a696
abrahamon <ipseabraham@gmail.com>
2016-11-18 18:49:24 -0600

    ruta  local para la base de datos

---
+++ WebService/WcfService1/App_Data/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfService1{
+    [DataContract]
+    public class Customer{
+        [DataMember]
+        public int ID_Card { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string LastName1 { get; set; }
+        [DataMember]
+        public string LastName2 { get; set; }
+        [DataMember]
+        public string Residence { get; set; }
+        [DataMember]
+        public string Nickname { get; set; }
+        [DataMember]
+        public string Secure_Pass { get; set; }
+        [DataMember]
+        public string BDate { get; set; }
+        [DataMember]
+        public int Phone { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public int PriorityLevel { get; set; }
+        [DataMember]
+        public bool Active { get; set; }
+    }
+}
